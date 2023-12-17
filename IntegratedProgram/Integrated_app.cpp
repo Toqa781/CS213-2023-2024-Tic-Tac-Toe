35c0faf6446d65b1bb49667cdaaa1025c0870888
ToqaAbdalla <132062106+Toqa781@users.noreply.github.com>
2023-12-17 22:13:38 +0200

    Update Integrated_app.cpp

--- IntegratedProgram/Integrated_app.cpp
+++ IntegratedProgram/Integrated_app.cpp
@@ -1,86 +1,101 @@
 #include<bits/stdc++.h>
 #include "BoardGame_Classes.h"
 using namespace std;
-    void playXO(){
-        int choice;
-        Player* players[2];
-        players[0] = new Player (1, 'x');
+void playXO(){
+    int choice;
+    Player* players[2];
+    players[0] = new Player (1, 'x');
 
-        cout << "Welcome to FCAI X-O Game. :)\n";
-        cout << "Press 1 if you want to play with computer: ";
-        cin >> choice;
-        if (choice != 1)
-            players[1] = new Player (2, 'o');
-        else
-            //Player pointer points to child
-            players[1] = new RandomPlayer ('o', 5);
+    cout << "Welcome to FCAI X-O Game. :)\n";
+    cout << "Press 1 if you want to play with computer: ";
+    cin >> choice;
+    if (choice != 1)
+        players[1] = new Player (2, 'o');
+    else
+        //Player pointer points to child
+        players[1] = new RandomPlayer ('o', 5);
 
-        GameManager x_o_game (new X_O_Board(), players);
-        x_o_game.run();
-        system ("pause");
+    GameManager x_o_game (new X_O_Board(), players);
+    x_o_game.run();
+    system ("pause");
+}
+void playPyramicTicTacToe(){
+    int choice;
+    Player* players[2];
+    players[0] = new Player (1, 'x');
+
+    cout << "Welcome to FCAI X-O Game. :)\n";
+    cout << "Press 1 if you want to play with computer: ";
+    cin >> choice;
+    if (choice != 1)
+        players[1] = new Player (2, 'o');
+    else
+        //Player pointer points to child
+        players[1] = new RandomPlayer ('o', 3);
+
+    GameManager x_o_game (new  Pyramic_X_O_Board(), players);
+    x_o_game.run();
+    system ("pause");
+}
+void playFourInARow(){
+    int choice;
+    Player* players[2];
+    players[0] = new Player (1, 'x');
+
+    cout << "Welcome to FCAI Four in a row Game. :)\n";
+    cout << "Press 1 if you want to play with computer and 2 if you want play with other player: ";
+    cin >> choice;
+    if (choice != 1)
+        players[1] = new Player (2, 'o');
+    else
+        //Player pointer points to child
+        players[1] = new RandomPlayer ('o', 3);
+
+    GameManager x_o_game (new four_in_a_row_Board(), players);
+    x_o_game.run();
+    system ("pause");
+}
+void play5x5TicTacToe(){
+    int choice;
+    Player* players[2];
+    players[0] = new Player (1, 'x');
+
+    cout << "Welcome to FCAI 5x5 Tic Tac Toe Game. :)\n";
+    cout << "Press 1 if you want to play with computer: ";
+    cin >> choice;
+    if (choice != 1)
+        players[1] = new Player (2, 'o');
+    else
+        //Player pointer points to child
+        players[1] = new RandomPlayer ('o', 3);
+
+    GameManager x_o_game (new TicTacToe5x5_Board(), players);
+    x_o_game.run();
+    system ("pause");
+
+}
+void IntegratedProgram(int choice){
+    if(choice==1){
+        playXO();
     }
-    void playPyramicTicTacToe(){
-
+    else if(choice==2){
+        playPyramicTicTacToe();
     }
-    void playFourInARow(){
-        int choice;
-        Player* players[2];
-        players[0] = new Player (1, 'x');
-
-        cout << "Welcome to FCAI Four in a row Game. :)\n";
-        cout << "Press 1 if you want to play with computer and 2 if you want play with other player: ";
-        cin >> choice;
-        if (choice != 1)
-            players[1] = new Player (2, 'o');
-        else
-            //Player pointer points to child
-            players[1] = new RandomPlayer ('o', 3);
-
-        GameManager x_o_game (new four_in_a_row_Board(), players);
-        x_o_game.run();
-        system ("pause");
+    else if(choice==3){
+        playFourInARow();
     }
-    void play5x5TicTacToe(){
-        int choice;
-        Player* players[2];
-        players[0] = new Player (1, 'x');
-
-        cout << "Welcome to FCAI 5x5 Tic Tac Toe Game. :)\n";
-        cout << "Press 1 if you want to play with computer: ";
-        cin >> choice;
-        if (choice != 1)
-            players[1] = new Player (2, 'o');
-        else
-            //Player pointer points to child
-            players[1] = new RandomPlayer ('o', 3);
-
-        GameManager x_o_game (new TicTacToe5x5_Board(), players);
-        x_o_game.run();
-        system ("pause");
-
+    else if(choice==4){
+        play5x5TicTacToe();
     }
-    void IntegratedProgram(int choice){
-        if(choice==1){
-            playXO();
-        }
-        else if(choice==2){
-            playPyramicTicTacToe();
-        }
-        else if(choice==3){
-            playFourInARow();
-        }
-        else if(choice==4){
-            play5x5TicTacToe();
-        }
-        else{
-            cout<<"Invalid choice"<<endl;
-        }
+    else{
+        cout<<"Invalid choice"<<endl;
     }
+}
 int main(){
     cout<<"Welcome to the Game"<<endl;
     cout<<"Choose the game you want to play"<<endl<<
-    "1- XO."<<endl<<"2- Pyramic Tic-Tac-Toe."<<endl<<"3-Four-in-a-row."<<endl
-    <<"4- 5 x 5 Tic Tac Toe."<<endl;
+        "1- XO."<<endl<<"2- Pyramic Tic-Tac-Toe."<<endl<<"3-Four-in-a-row."<<endl
+        <<"4- 5 x 5 Tic Tac Toe."<<endl;
     int choice;
     cin>>choice;
     IntegratedProgram(choice);
