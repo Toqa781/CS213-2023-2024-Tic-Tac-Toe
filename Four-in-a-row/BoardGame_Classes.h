ca5de085fc1d40915c82a327af3c15a502bc0b4e
ToqaAbdalla <132062106+Toqa781@users.noreply.github.com>
2023-12-17 13:56:48 +0200

    Update BoardGame_Classes.h

--- Four-in-a-row/BoardGame_Classes.h
+++ Four-in-a-row/BoardGame_Classes.h
@@ -1,119 +1,103 @@
-// Class definition for X_O_Board class
+// Class definition for XO game classes
 // Author:  Mohammad El-Ramly
 // Date:    10/10/2022
 // Version: 1
-#include <iostream>
-#include <random>
-#include <iomanip>
-#include <algorithm>
-#include "BoardGame_Classes.h"
+#ifndef _BoardGame_CLASSES_H
+#define _BoardGame_CLASSES_H
+#include<bits/stdc++.h>
 using namespace std;
 
-// Set the board
-X_O_Board::X_O_Board () {
-    n_rows =6;
-    n_cols = 7;
-    board = new char*[n_rows];
-    for (int i = 0; i < n_rows; i++) {
-        board [i] = new char[n_cols];
-        for (int j = 0; j < n_cols; j++)
-            board[i][j] = 0;
-    }
-}
+class Board {
+protected:
+    int n_rows, n_cols;
+    char** board;
+    int  n_moves = 0;
 
-// Return true  if move is valid and put it on board
-// within board boundaries in empty cell
-// Return false otherwise
-bool X_O_Board::update_board (int x, int y, char mark){
-    // Only update if move is valid
-    if(y<0 || y>=n_cols){
-        return false;
-    }
-    // find the lowest available row in the selected column
-    x=n_rows-1;
-    while(x>=0&& board[x][y]!=0){
-        x--;
-    }
-    //if the column is full return false
-    if(x<0){
-        return false;
-    }
-    //place the mark at the lowest available position in the column
-    board[x][y]=toupper(mark);
-    n_moves++;
-    return true;
-}
+public:
+    // Return true  if move is valid and put it on board
+    // within board boundaries in empty cell
+    // Return false otherwise
+    virtual bool update_board (int x, int y, char symbol) = 0;
+    // Returns true if there is any winner
+    // either X or O
+    // Written in a complex way. DO NOT DO LIKE THIS.
+    virtual bool is_winner() = 0;
+    // Return true if all moves are done and no winner
+    virtual bool is_draw() = 0;
+    // Display the board and the pieces on it
+    virtual void display_board() = 0;
+    // Return true if game is over
+    virtual bool game_is_over() = 0;
+};
 
-// Display the board and the pieces on it
-void X_O_Board::display_board() {
-    for (int i: {0,1,2,3,4,5}) {
-        cout << "\n| ";
-        for (int j: {0,1,2,3,4,5,6}) {
-            cout << "(" << i << "," << j << ")";
-            cout << setw(2) << board [i][j] << " |";
-        }
-        cout << "\n-----------------------------------------------------------------";
-    }
-    cout << endl;
-}
+///////////////////////////////////////////
+// This class represents a 3 x 3 board
+// used in X_O game
+class four_in_a_row_Board:public Board {
+public:
+    four_in_a_row_Board ();
+    bool update_board (int x, int y, char mark);
+    void display_board();
+    bool is_winner();
+    bool is_draw();
+    bool game_is_over();
+};
 
-// Returns true if there is any winner
-// either X or O
-// Written in a complex way. DO NOT DO LIKE THIS.
-bool X_O_Board::is_winner() {
-    //check horizontally
-    for(int row=0;row<n_rows;row++){
-        for(int col=0;col<=n_cols-4;col++){
-            if(board[row][col]!=0 &&
-               board[row][col]==board[row][col+1]&&
-               board[row][col]==board[row][col+2]&&
-               board[row][col]==board[row][col+3]){
-                return true;
-            }
-        }
-    }
-    //check vertically
-    for(int col=0;col<n_cols;col++){
-        for(int row=0;row<=n_rows-4;row++){
-            if (board[row][col] != 0 &&
-                board[row][col] == board[row + 1][col] &&
-                board[row][col] == board[row + 2][col] &&
-                board[row][col] == board[row + 3][col]) {
-                return true;
-            }
-        }
-    }
-    //check diagonally(ascending)
-    for (int row = 3; row < n_rows; ++row) {
-        for (int col = 0; col <= n_cols - 4; ++col) {
-            if (board[row][col] != 0 &&
-                board[row][col] == board[row - 1][col + 1] &&
-                board[row][col] == board[row - 2][col + 2] &&
-                board[row][col] == board[row - 3][col + 3]) {
-                return true;
-            }
-        }
-    }
-    // Check diagonally (descending)
-    for (int row = 0; row <= n_rows - 4; ++row) {
-        for (int col = 0; col <= n_cols - 4; ++col) {
-            if (board[row][col] != 0 &&
-                board[row][col] == board[row + 1][col + 1] &&
-                board[row][col] == board[row + 2][col + 2] &&
-                board[row][col] == board[row + 3][col + 3]) {
-                return true;
-            }
-        }
-    }
+///////////////////////////////////////////
+// This class represents a player who has
+// a name and a symbol to put on board
+class Player {
+protected:
+    string name;
+    char symbol;
+public:
+    // Two constructors to initiate player
+    // Give player a symbol to use in playing
+    // It can be X or O or others
+    // Optionally, you can give him ID or order
+    // Like Player 1 and Player 2
+    Player (char symbol); // Needed for computer players
+    Player (int order, char symbol);
+    // Get desired move: x y (each between 0 and 2)
+    // Virtual (can change for other player types)
+    virtual void get_move(int& x, int& y);
+    // Give player info as a string
+    string to_string();
+    // Get symbol used by player
+    char get_symbol();
+};
 
-    return false;
-}
+///////////////////////////////////////////
+// This class represents a random computer player
+// that generates random positions x y (0 to 2)
+// If invalid, game manager asks to regenerate
+class RandomPlayer: public Player {
+protected:
+    int dimension;
+public:
+    // Take a symbol and pass it to parent
+    RandomPlayer (char symbol, int dimension);
+    // Generate a random move
+    void get_move(int& x, int& y);
+};
 
-// Return true if 9 moves are done and no winner
-bool X_O_Board::is_draw() {
-    return (n_moves == n_rows*n_cols);
-}
+///////////////////////////////////////////
+class GameManager {
+private:
+    Board* boardPtr;
+    Player* players[2];
+public:
+    GameManager(Board*, Player* playerPtr[2]);
+    void run();
+    // This method creates board and players
+    // It displays board
+    // While True
+    //   For each player
+    //      It takes a valid move as x, y pair (between 0 - 2)
+    //      It updates board and displays otit
+    //      If winner, declare so and end
+    //      If draw, declare so and end
 
-bool X_O_Board::game_is_over () {
-    return is_winner()||is_draw();
-}
+};
+
+#endif
