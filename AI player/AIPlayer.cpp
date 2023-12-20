a91e3479278b52bc3041ba9509228870482b5c1c
habibaaymannn <145665685+habibaaymannn@users.noreply.github.com>
2023-12-20 22:12:04 +0200

    Add files via upload

---
+++ AI player/AIPlayer.cpp
@@ -0,0 +1,9 @@
+#include "BoardGame_Classes.hpp"
+
+AIPlayer::AIPlayer(char symbol,Board* board):Player(symbol),board(board){
+    this->name = "AI player";
+    cout<<"My name is "<<name<<'\n';
+}
+void AIPlayer::get_move(int &x, int &y) {
+    board->MM(x,y,false,true);
+}
