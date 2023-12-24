6a71a4de7d856a9cf628667348fba8cffaf2cd59
habibaaymannn <145665685+habibaaymannn@users.noreply.github.com>
2023-12-24 13:09:47 +0200

    Update AIPlayer.cpp

--- AI player/AIPlayer.cpp
+++ AI player/AIPlayer.cpp
@@ -4,6 +4,6 @@ AIPlayer::AIPlayer(char symbol,Board* board):Player(symbol),board(board){
     this->name = "AI player";
     cout<<"My name is "<<name<<'\n';
 }
-void AIPlayer::get_move(int &x, int &y) {
+void AIPlayer::get_move(int& x, int& y) {
     board->MM(x,y,false,true);
 }
